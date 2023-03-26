9c308c3d74d8f2d4c5c5b739c8749cd35277e476
Efrén Corzón Vázquez <efren@Mac-mini.local>
2023-03-26 20:43:45 +0200

    proyecto funcionando

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const a = document.querySelector("altura");
-const p = document.querySelector("peso");
-// const boton = document.querySelector("button");
-const parrafoResultado = document.querySelector("mostrarResultado");
-const encabezado = document.querySelector("h1");
+const boton = document.querySelector("button");
+const parrafoResultado = document.querySelector("#mostrarResultado");
+const a = document.querySelector("#altura");
+const p = document.querySelector("#peso");
+
+//Función Botón.
+const mandarFormulario = () => {
+  boton.addEventListener("click", imc);
+};
 
 //Función para calcular en IMC.
-const imc = (a, p) => {
-  let r = (p / Math.pow(a / 100, 2)).toFixed(1);
+const imc = (e) => {
+  e.preventDefault();
+  let r = (p.value / Math.pow(a.value / 100, 2)).toFixed(1);
+
+  //Posibles casos según resultado.
+  if (r < 18.5) {
+    parrafoResultado.innerHTML = `Tu IMC es: ${r} Tu peso es inferior al normal`;
+  } else if (r >= 18.5 && r < 24.9) {
+    parrafoResultado.innerHTML = `Tu IMC es: ${r} Tu peso es normal`;
+  } else if (r >= 25 && r < 29.9) {
+    parrafoResultado.innerHTML = `Tu IMC es: ${r} Tu peso es superior al normal`;
+  } else if (r > 30) {
+    parrafoResultado.innerHTML = `Tu IMC es: ${r} 
+    Tienes obesidad`;
+  }
 };
 
-const prueba = () => {
-  encabezado.addEventListener("click", () => {
-    console.log("hola");
-  });
-};
-
-prueba();
-
-/* let a = prompt("¿Cual es tu aaltura?");
-let p = prompt("¿Cual es tu peso?"); */
-//Posibles casos según resultado.
-/*  if (r < 18.5) {
-  `Tu IMC es: ${r} Tu p es inferior al normal`;
-} else if (r >= 18.5 && r < 24.9) {
-  `Tu IMC es: ${r} Tu p es normal`;
-} else if (r >= 25 && r < 29.9) {
-  `Tu IMC es: ${r} Tu p es superior al normal`;
-} else if (r > 30) {
-  `Tu IMC es: ${r} Tienes obesidad`;
-} */
+mandarFormulario();
